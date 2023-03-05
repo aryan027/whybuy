d3f5a74030689504b563862f1fa8eb9ff0c25100
mbackenddev <mbackenddev@gmail.com>
2023-03-05 18:07:10 +0530

    change app name in mater title

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>Fingser - @yield('title')</title>
+    <title>whybuy - @yield('title')</title>
     
     <link rel="stylesheet" href="{{asset('assets/css/main/app.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/main/app-dark.css')}}">
