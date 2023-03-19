b986cd16368a8366832fcbd6900d18fef3413dab
Anuj Chauhan <anujchauhan0996@gmail.com>
2023-03-19 16:09:08 +0530

    google id

---
+++ app/Http/Controllers/Api/ReviewController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Review;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ReviewController extends Controller
+{
+    public function reveiw(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'ad_id'=>'required',
+            'rating' => 'required|integer|between:1,5',
+            'review' => 'required|string|max:255',
+        ]);
+        if ($validator->fails()) {
+            return $this->ErrorResponse(200,$validator->errors()->first());
+        }
+        if (Review::where(['user_id'=>auth()->id(),'ad_id'=> $request->ad_id])->exists()) {
+            return $this->ErrorResponse(200,'You have already submitted a review ');
+        }
+        // Create the new app review
+        $review = Review::create([
+            'user_id'=>auth()->id(),
+            'rating'=>$request['rating'],
+            'comment'=>$request['comment'],
+            'ad_id'=>$request['ad_id']
+        ]);
+        return $this->SuccessResponse(200,'Review given successfully ..!',$review);
+
+    }
+
+    public function review_list(Request $request){
+        $validator = Validator::make($request->all(), [
+            'ad_id'=>'required',
+        ]);
+        if ($validator->fails()) {
+            return $this->ErrorResponse(200,$validator->errors()->first());
+        }
+        $list= Review::with('ads')->where(['ad_id'=>$request->ad_id])->get();
+        return $this->SuccessResponse(200,'Data fetch successfully ..!',$list);
+    }
+    public function review_user_list(Request $request){
+        $validator = Validator::make($request->all(), [
+            'ad_id'=>'required',
+        ]);
+        if ($validator->fails()) {
+            return $this->ErrorResponse(200,$validator->errors()->first());
+        }
+        $list= Review::with('ads')->where(['ad_id'=>$request->ad_id,'user_id'=>auth()->id()])->get();
+        return $this->SuccessResponse(200,'Data fetch successfully ..!',$list);
+    }
+}
